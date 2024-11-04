135f01067ce1474218bb447ff72374bc3f1c1e21
Isac Alves Dias <isac3299@gmail.com>
2024-11-04 11:56:40 -0300

    fix: Fixed the jump of the player.

--- SpatialEndlessRunner/Assets/MyProject/Scripts/PlayerController.cs
+++ SpatialEndlessRunner/Assets/MyProject/Scripts/PlayerController.cs
@@ -46,13 +46,13 @@ public class PlayerController : MonoBehaviour
             animator.SetBool("OnGround", false);
         }
 
-        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S) && !OnGround(OnGroundCheck.position, jumpRadius, onGround))
+        if (Input.GetKeyDown(KeyCode.S) && !OnGround(OnGroundCheck.position, jumpRadius, onGround))
         {
             canForceDown = true;
         }
 
         //check if can jump
-        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W) && OnGround(OnGroundCheck.position, jumpRadius, onGround))
+        if (Input.GetKeyDown(KeyCode.UpArrow) && OnGround(OnGroundCheck.position, jumpRadius, onGround))
         {
             canJump = true;
         }
